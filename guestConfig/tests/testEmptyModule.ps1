cbb1f649e703f8186db18ef9c326ff99a4c395bf
jchancellor-ms <jchancellor@microsoft.com>
2023-06-13 08:51:00 -0700

    added empty module for testing

--- guestConfig/tests/testEmptyModule.ps1
+++ guestConfig/tests/testEmptyModule.ps1
@@ -3,5 +3,6 @@ $Params = @{
     ensure = "Present"
 }
 
+import-Module ./modules/emptyModule/
 Invoke-DscResource @Params -Method Get
 #Get-GuestConfigurationPackageComplianceStatus -Path ./packages/installAzCliLinux.zip -Parameter $Params
