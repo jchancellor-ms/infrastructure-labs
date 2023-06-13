b86483b7cde5994cee3a42d52f2cd2ba65618b18
jchancellor-ms <jchancellor@microsoft.com>
2023-06-13 08:16:09 -0700

    added empty module for testing

---
+++ guestConfig/modules/emptyModule/emptyModule.psm1
@@ -0,0 +1,64 @@
+[DscResource()]
+class emptyModule {
+    [DscProperty(Key)]
+    [string] $name = "test"
+
+    [DscProperty(Mandatory)]
+    [emptyModuleEnsure] $ensure = [emptyModuleEnsure]::Present
+
+    [DscProperty(NotConfigurable)]
+    [installAzCliReason[]] $reasons = [installAzCliReason[]]::new()
+
+
+
+
+    # class constructor
+    # Get() method
+    [emptyModule] Get() {        
+
+        # Create the constructor
+        $currentState = [emptyModule]::new()
+        $currentState.name = $this.name
+        $currentState.ensure = $this.ensure
+        $currentState.reasons += [installAzCliReason[]]@{
+            Code = "$($this.name):emptyModule:NotInstalled"
+            Phrase = "This is an empty module for testing."
+        } 
+
+        return $currentState
+    }
+
+    [bool] Test() {
+        # Test the current state of the resource against the desired state
+        $currentState = $this.Get()
+
+        return $false
+    }
+
+    [void] Set() {
+        if ($this.Test()) {
+            return
+        }
+
+        Write-Output "This is a test output"
+        Write-Host "This is a test screen output"
+
+    }
+       
+}
+
+enum emptyModuleEnsure
+{
+    Absent
+    Present
+}
+
+#define a class resource for the Reason property
+class installAzCliReason {
+    [DscProperty()]
+    [string] $Code
+
+    [DscProperty()]
+    [string] $Phrase
+}
+
