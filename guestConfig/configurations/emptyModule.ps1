da6d11d003db9902d4036f63e99c1d54b085649a
jchancellor-ms <jchancellor@microsoft.com>
2023-06-13 08:20:42 -0700

    added empty module for testing

---
+++ guestConfig/configurations/emptyModule.ps1
@@ -0,0 +1,20 @@
+param(
+    [string[]]$outDirectory = "./compiledConfigurations"
+)
+
+$outFullPath = "$outDirectory/emptyModule"
+
+If (!(Test-Path -PathType container $outFullPath)) {
+    New-Item -ItemType Directory -Path $outFullPath
+}
+
+Configuration emptyModule {
+    Import-DscResource -ModuleName 'emptyModule'
+
+        emptyModule thisLinuxMachine {
+            name = "emptyModule"
+            ensure = "Present"
+        }
+}
+
+emptyModule -out $outFullPath
